9b25588d8a3496ce07fce1b0ba5526385d25c91d
GeorgeClark <GeorgeClark@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-10-04 15:45:29 +0000

    Item9774: update build.pl so that build will use the extension module for RELEASE/VERSION strings
    git-svn-id: http://svn.foswiki.org/trunk/HtmlFormsPlugin@9499 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/TWiki/Plugins/HtmlFormsPlugin/build.pl
+++ lib/TWiki/Plugins/HtmlFormsPlugin/build.pl
@@ -31,7 +31,7 @@ our @ISA = qw( TWiki::Contrib::Build );
 
 sub new {
     my $class = shift;
-    return bless( $class->SUPER::new( "HtmlFormsPlugin", "Build" ), $class );
+    return bless( $class->SUPER::new( "HtmlFormsPlugin" ), $class );
 }
 
 # Example: Override the build target
